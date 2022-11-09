923eb2c19eed372dc064d7426ca9b81d3052a9d0
jiangyy <jyydev@163.com>
2022-11-09 14:19:22 +0800

    harmonyos

---
+++ entry/src/ohosTest/java/com/jiangyy/wanandroid/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.jiangyy.wanandroid;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.jiangyy.wanandroid", actualBundleName);
+    }
+}
---
+++ entry/src/test/java/com/jiangyy/wanandroid/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.jiangyy.wanandroid;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
