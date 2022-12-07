5320f223825b8ab02487c9ed7773c3426282ac0d
jiangyy <jyydev@163.com>
2022-12-07 11:42:54 +0800

    使用Retrofit+ViewModel

--- app/src/main/java/com/jiangyy/wanandroid/logic/ApiService.kt
+++ app/src/main/java/com/jiangyy/wanandroid/logic/ApiService.kt
@@ -1,15 +1,26 @@
 package com.jiangyy.wanandroid.logic
 
 import com.jiangyy.wanandroid.entity.Article
+import com.jiangyy.wanandroid.entity.UserInfo
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ApiService {
+
+    @POST("user/logout/json")
+    suspend fun logout(): Bean<Any>
 
     @GET("article/list/{page}/json")
     suspend fun pageHomeArticle(@Path("page") page: Int): Bean<PageData<Article>>
 
     @GET("article/listproject/{page}/json")
     suspend fun pageHomeProject(@Path("page") page: Int): Bean<PageData<Article>>
+
+    @GET("user/lg/userinfo/json")
+    suspend fun infoUser(): Bean<UserInfo>
+
+    @GET("message/lg/count_unread/json")
+    suspend fun getUnreadMessageCount(): Bean<Int>
 
 }
