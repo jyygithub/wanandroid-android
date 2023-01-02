109f2166d2fa3da841e58c1676e5a5186eb7504f
jiangyy <jyydev@163.com>
2023-01-02 10:17:36 +0800

    文章取消分享

--- app/src/main/java/com/jiangyy/wanandroid/ui/user/ShareHistoryFragment.kt
+++ app/src/main/java/com/jiangyy/wanandroid/ui/user/ShareHistoryFragment.kt
@@ -2,12 +2,17 @@ package com.jiangyy.wanandroid.ui.user
 
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.jiangyy.core.doneToast
+import com.jiangyy.core.errorToast
+import com.jiangyy.dialog.StringBottomListDialog
 import com.jiangyy.wanandroid.ui.BaseArticlesFragment
 import kotlinx.coroutines.launch
 
 class ShareHistoryFragment private constructor() : BaseArticlesFragment() {
 
     private val mViewModel by viewModels<ShareHistoryViewModel>()
+
+    private var mCurrentPosition = 0
 
     override fun initObserver() {
 
@@ -16,29 +21,29 @@ class ShareHistoryFragment private constructor() : BaseArticlesFragment() {
                 mAdapter.submitData(pagingData)
             }
         }
-//        mViewModel.unshare.observe(this) {
-//            if (it.isSuccess) {
-//                doneToast("取消分享成功")
-//                mAdapter.removeAt(mCurrentPosition)
-//            } else {
-//                errorToast(it.exceptionOrNull()?.message.orEmpty())
-//            }
-//        }
+        mViewModel.unshare.observe(this) {
+            if (it.isSuccess) {
+                doneToast("取消分享成功")
+                mAdapter.removeAt(mCurrentPosition)
+            } else {
+                errorToast(it.exceptionOrNull()?.message.orEmpty())
+            }
+        }
     }
 
     override fun initWidget() {
-//        mAdapter.setOnItemLongClickListener { _, _, position ->
-//            mCurrentPosition = position
-//            StringBottomListDialog()
-//                .bindConfig { title = "文章操作" }
-//                .items("取消分享") { _, _ ->
-//                    val article = mAdapter.getItem(position)
-//                    mViewModel.unshare(article.id.orEmpty())
-//                }
-//                .show(childFragmentManager)
-//
-//            false
-//        }
+        mAdapter.setOnItemLongClickListener {  position ->
+            mCurrentPosition = position
+            StringBottomListDialog()
+                .bindConfig { title = "文章操作" }
+                .items("取消分享") { _, _ ->
+                    val article = mAdapter.peek(position)
+                    mViewModel.unshare(article?.id.orEmpty())
+                }
+                .show(childFragmentManager)
+
+            false
+        }
 
     }
 
