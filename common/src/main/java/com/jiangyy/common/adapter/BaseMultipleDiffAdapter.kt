bfd43fd6043a54ba0e5b6a55d1c83b157428ccdc
jiangyy <jyydev@163.com>
2023-03-05 11:12:54 +0800

    update

--- common/src/main/java/com/jiangyy/common/adapter/BaseMultipleDiffAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.jiangyy.common.adapter
-
-import androidx.viewbinding.ViewBinding
-
-abstract class BaseMultipleDiffAdapter<T : Any>(id: (T) -> Any? = { it }) : BaseDiffAdapter<T, ViewBinding>(id) {
-
-}
--- common/src/main/java/com/jiangyy/common/statusview/StatusModule.kt
+++
@@ -1,7 +0,0 @@
-package com.jiangyy.common.statusview
-
-import android.view.View
-
-interface StatusModule {
-    val viewBindStatus: View
-}
