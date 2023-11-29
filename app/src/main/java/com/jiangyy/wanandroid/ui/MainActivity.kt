fd10de2382a9e5ada5b65f197c58d540fbcb8cb3
jiangyy <jiangyy@petalmail.com>
2023-11-29 10:42:30 +0800

    包结构优化

---
+++ app/src/main/java/com/jiangyy/wanandroid/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.jiangyy.wanandroid.ui
+
+import com.jiangyy.wanandroid.base.BaseActivity
+import com.jiangyy.wanandroid.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+}
