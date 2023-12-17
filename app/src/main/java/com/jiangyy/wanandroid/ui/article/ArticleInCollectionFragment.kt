1da66444953e49f2e93c83f5f5cd4369e2eb1108
jiangyy <jiangyy@petalmail.com>
2023-12-17 09:51:57 +0800

    Article

---
+++ app/src/main/java/com/jiangyy/wanandroid/ui/article/ArticleInCollectionFragment.kt
@@ -0,0 +1,23 @@
+package com.jiangyy.wanandroid.ui.article
+
+import com.jiangyy.wanandroid.data.Api
+import com.jiangyy.wanandroid.data.ApiResponse
+import com.jiangyy.wanandroid.data.RetrofitHelper
+import com.jiangyy.wanandroid.entity.Article
+import com.jiangyy.wanandroid.ui.BaseArticleFragment
+
+/**
+ * 收藏历史
+ */
+class ArticleInCollectionFragment : BaseArticleFragment() {
+
+    override suspend fun revoke(page: Int): ApiResponse<ApiResponse.Paging<Article>> {
+        return RetrofitHelper.getInstance().create(Api::class.java).listCollect(page)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ArticleInCollectionFragment()
+    }
+
+}
