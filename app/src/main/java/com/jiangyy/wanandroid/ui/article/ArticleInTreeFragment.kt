33440504211d1f39c83cb0538c417438769d4e72
jiangyy <jiangyy@petalmail.com>
2023-12-23 09:52:28 +0800

    Article

---
+++ app/src/main/java/com/jiangyy/wanandroid/ui/article/ArticleInTreeFragment.kt
@@ -0,0 +1,24 @@
+package com.jiangyy.wanandroid.ui.article
+
+import com.koonny.appcompat.core.intentParcelable
+import com.jiangyy.wanandroid.data.Api
+import com.jiangyy.wanandroid.data.ApiResponse
+import com.jiangyy.wanandroid.data.RetrofitHelper
+import com.jiangyy.wanandroid.entity.Article
+import com.jiangyy.wanandroid.entity.Tree
+import com.jiangyy.wanandroid.ui.BaseArticleFragment
+
+class ArticleInTreeFragment : BaseArticleFragment() {
+
+    private val mTree by intentParcelable<Tree>("tree")
+
+    override suspend fun revoke(page: Int): ApiResponse<ApiResponse.Paging<Article>> {
+        return RetrofitHelper.getInstance().create(Api::class.java).pageArticleInTree(page, mTree?.id.orEmpty())
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ArticleInTreeFragment()
+    }
+
+}
