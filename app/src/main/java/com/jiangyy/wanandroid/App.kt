8ce7ecccbaecf1d3173158cc7a1bf25cbd9812f4
jiangyy <jiangyy@petalmail.com>
2024-02-18 11:05:42 +0800

    update

--- app/src/main/java/com/jiangyy/wanandroid/App.kt
+++ app/src/main/java/com/jiangyy/wanandroid/App.kt
@@ -1,6 +1,21 @@
 package com.jiangyy.wanandroid
 
 import android.app.Application
+import android.content.ContextWrapper
+import timber.log.Timber
+
+private lateinit var INSTANCE: Application
+
+object AppContext : ContextWrapper(INSTANCE)
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
 }
