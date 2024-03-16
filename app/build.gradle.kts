44c3c21fbbab6db8948819dd1a952f548f7f2e0d
jiangyy <jiangyy@petalmail.com>
2024-03-16 11:09:58 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.googleKsp)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -11,9 +13,8 @@ android {
         applicationId = "com.jiangyy.wanandroid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = 5
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -21,9 +22,6 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-        debug {
-            applicationIdSuffix = ".debug"
         }
     }
     compileOptions {
@@ -43,11 +41,28 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.leakcanary)
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(libs.appcompat)
+    implementation(libs.google.android.material)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(libs.recyclerview)
+    implementation(libs.swiperefreshlayout)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("com.guolindev.permissionx:permissionx:1.7.1")
+    implementation("io.coil-kt:coil:2.2.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.franmontiel:PersistentCookieJar:v1.0.1")
+    implementation("com.github.Justson.AgentWeb:agentweb-core:v4.1.9-androidx")
+    implementation("io.github.cymchad:BaseRecyclerViewAdapterHelper:4.0.0-beta04")
+    implementation("com.koonny.appcompat:appcompat:1.0.0-SNAPSHOT")
+    implementation("com.koonny.dialog:dialog:1.0.0-SNAPSHOT")
+    implementation(libs.timber)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
