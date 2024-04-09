46e6888e897eb56cc9969383c268bf1f1a34ad29
jiangyy <jiangyy@petalmail.com>
2024-04-09 16:45:09 +0800

    update

---
+++ app/src/main/java/com/jiangyy/wanandroid/ui/about/AboutActivity.kt
@@ -0,0 +1,7 @@
+package com.jiangyy.wanandroid.ui.about
+
+import com.jiangyy.wanandroid.base.BaseActivity
+import com.jiangyy.wanandroid.databinding.ActivityAboutBinding
+
+class AboutActivity : BaseActivity<ActivityAboutBinding>(ActivityAboutBinding::inflate) {
+}
