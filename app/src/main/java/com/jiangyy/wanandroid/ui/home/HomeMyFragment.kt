5b49977a79686e3b7dc17a46dba1fb99cd906da9
jiangyy <jiangyy@petalmail.com>
2024-04-17 10:46:47 +0800

    update

--- app/src/main/java/com/jiangyy/wanandroid/ui/home/HomeMyFragment.kt
+++ app/src/main/java/com/jiangyy/wanandroid/ui/home/HomeMyFragment.kt
@@ -2,8 +2,17 @@ package com.jiangyy.wanandroid.ui.home
 
 import com.jiangyy.wanandroid.base.BaseFragment
 import com.jiangyy.wanandroid.databinding.FragmentHomeMyBinding
+import com.jiangyy.wanandroid.kit.click
+import com.jiangyy.wanandroid.ui.user.LoginActivity
 
 class HomeMyFragment : BaseFragment<FragmentHomeMyBinding>(FragmentHomeMyBinding::inflate) {
+
+    override fun onPrepareWidget() {
+        super.onPrepareWidget()
+        binding.ivAvatar.click {
+            LoginActivity.actionStart(requireActivity())
+        }
+    }
 
     companion object {
         @JvmStatic
--- app/src/main/java/com/jiangyy/wanandroid/ui/user/LoginActivity.kt
+++ app/src/main/java/com/jiangyy/wanandroid/ui/user/LoginActivity.kt
@@ -3,15 +3,33 @@ package com.jiangyy.wanandroid.ui.user
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.jiangyy.wanandroid.base.BaseActivity
 import com.jiangyy.wanandroid.databinding.ActivityLoginBinding
+import com.jiangyy.wanandroid.kit.click
+import com.jiangyy.wanandroid.ktor.UserApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onPrepareWidget() {
+        super.onPrepareWidget()
+        binding.btnLogin.click {
+            login()
+        }
     }
 
+    private fun login() {
+        lifecycleScope.launch {
+            flow {
+                emit(UserApi().login("", ""))
+            }.catch { }.collect {
+
+            }
+        }
+    }
 
     companion object {
         fun actionStart(context: Context) {
