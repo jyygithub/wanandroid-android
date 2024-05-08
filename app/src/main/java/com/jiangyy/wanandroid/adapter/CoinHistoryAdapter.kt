dbfe0f65e386e0445a3312535be76f114892f2c4
jiangyy <jiangyy@petalmail.com>
2024-05-08 09:28:52 +0800

    update

---
+++ app/src/main/java/com/jiangyy/wanandroid/adapter/CoinHistoryAdapter.kt
@@ -0,0 +1,25 @@
+package com.jiangyy.wanandroid.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.chad.library.adapter4.BaseQuickAdapter
+import com.jiangyy.wanandroid.databinding.RecyclerItemCoinHistoryBinding
+import com.jiangyy.wanandroid.entity.CoinHistory
+
+class CoinHistoryAdapter : BaseQuickAdapter<CoinHistory, CoinHistoryAdapter.VH>() {
+
+    class VH(val binding: RecyclerItemCoinHistoryBinding) : ViewHolder(binding.root)
+
+    override fun onBindViewHolder(holder: VH, position: Int, item: CoinHistory?) {
+        holder.binding.tvCoin.text = "+${item?.coinCount}"
+        holder.binding.tvDate.text = ""
+        holder.binding.tvType.text = item?.reason
+    }
+
+    override fun onCreateViewHolder(context: Context, parent: ViewGroup, viewType: Int): VH {
+        return VH(RecyclerItemCoinHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+}
