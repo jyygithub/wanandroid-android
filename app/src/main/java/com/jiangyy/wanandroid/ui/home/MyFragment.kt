1b7fb0aa600fc16fe99ff4a959cf0b7a38f62d72
jiangyy <jiangyy@petalmail.com>
2024-05-17 15:13:20 +0800

    update

--- app/src/main/java/com/jiangyy/wanandroid/ui/home/MyFragment.kt
+++ app/src/main/java/com/jiangyy/wanandroid/ui/home/MyFragment.kt
@@ -1,16 +1,20 @@
 package com.jiangyy.wanandroid.ui.home
 
+import android.view.View
+import com.chad.library.adapter4.BaseQuickAdapter
 import com.chad.library.adapter4.QuickAdapterHelper
 import com.jiangyy.wanandroid.R
 import com.jiangyy.wanandroid.adapter.MyBottomItem
 import com.jiangyy.wanandroid.adapter.MyContentAdapter
 import com.jiangyy.wanandroid.adapter.MyHeadAdapter
+import com.jiangyy.wanandroid.adapter.MyItem
 import com.jiangyy.wanandroid.adapter.MyRoundItem
 import com.jiangyy.wanandroid.adapter.MyTopItem
 import com.jiangyy.wanandroid.base.BaseFragment
 import com.jiangyy.wanandroid.databinding.FragmentMyBinding
+import com.jiangyy.wanandroid.kit.toast
 
-class MyFragment : BaseFragment<FragmentMyBinding>(FragmentMyBinding::inflate) {
+class MyFragment : BaseFragment<FragmentMyBinding>(FragmentMyBinding::inflate), BaseQuickAdapter.OnItemClickListener<MyItem> {
 
     private val mMyHeadAdapter = MyHeadAdapter()
     private val mMyContentAdapter = MyContentAdapter()
@@ -24,18 +28,49 @@ class MyFragment : BaseFragment<FragmentMyBinding>(FragmentMyBinding::inflate) {
         binding.recyclerView.adapter = mHelper.adapter
 
         mMyHeadAdapter.item = ""
-
+        mMyContentAdapter.setOnItemClickListener(this)
         mMyContentAdapter.submitList(
             listOf(
-                MyTopItem(R.drawable.ic_home, "我的分享", ""),
-                MyBottomItem(R.drawable.ic_home, "我的收藏", ""),
-                MyRoundItem(R.drawable.ic_home, "知识体系", ""),
-                MyTopItem(R.drawable.ic_home, "我的积分", "0"),
-                MyBottomItem(R.drawable.ic_home, "排行榜", ""),
-                MyTopItem(R.drawable.ic_home, "当前版本", "v1.0.0"),
-                MyBottomItem(R.drawable.ic_home, "关于", ""),
+                MyTopItem(R.drawable.ic_public_share, "我的分享", ""),
+                MyBottomItem(R.drawable.ic_public_favor, "我的收藏", ""),
+                MyRoundItem(R.drawable.ic_public_storage, "知识体系", ""),
+                MyTopItem(R.drawable.ic_public_highlights, "我的积分", "0"),
+                MyBottomItem(R.drawable.ic_desktop_servicewidgets, "排行榜", ""),
+                MyTopItem(R.drawable.ic_public_upgrade, "当前版本", "v1.0.0"),
+                MyBottomItem(R.drawable.ic_gallery_cloud_synchronization, "开源地址", "Github"),
             )
         )
     }
 
+    override fun onClick(adapter: BaseQuickAdapter<MyItem, *>, view: View, position: Int) {
+        when (position) {
+            0 -> {
+                toast("分享")
+            }
+
+            1 -> {
+                toast("收藏")
+            }
+
+            2 -> {
+                toast("知识体系")
+            }
+
+            3 -> {
+                toast("积分")
+            }
+
+            4 -> {
+                toast("排行榜")
+            }
+
+            5 -> {
+                toast("版本")
+            }
+
+            6 -> {
+                toast("开源地址")
+            }
+        }
+    }
 }
